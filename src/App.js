b1f266e6e9e376b457fc2b11cdd757b1a485bd6b
Koseohyeon <ksh030818@naver.com>
2024-05-28 16:37:00 +0900

    링크반리엑트 수업

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import MemberSave from "./pages/MemberSave";
+import Login from "./pages/Login";
+import MemberList from "./pages/MemberList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" exact={true} element={<Home />} />
+          <Route path="/memberSave" exact={true} element={<MemberSave />} />
+          <Route path="/login" exact={true} element={<Login />} />
+          <Route path="/memberList" exact={true} element={<MemberList />} />
+        </Routes>
+      </Container>
     </div>
   );
 }
---
+++ src/pages/MemberSave.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MemberSave=()=>{
+  return (
+    <div>
+      <div>
+        <h1>회원등록 페이지</h1>
+      </div>
+    </div>
+  )
+}
+export default MemberSave;
