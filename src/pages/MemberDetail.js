ac84b1aed99060cd4ea02930d40f877897a593e5
Koseohyeon <ksh030818@naver.com>
2024-06-04 16:37:40 +0900

    수정중

---
+++ src/pages/MemberDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MemberDetail = () => {
+    return (
+        <div>
+            <h1>디테일페이지</h1>
+        </div>
+    );
+};
+
+export default MemberDetail;
--- src/pages/MemberList.js
+++ src/pages/MemberList.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Member from '../components/Member';
+const MemberList = () => {
 
-export default function MemberList() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const [member,setMembers]= useState([]);
+    useEffect(()=>{
+      //초기화
+
+    },[]);
+
+
+    return (
+      <div>
+        <h1>회원 리스트 페이지</h1>
+       {
+        MemberSave.map((member) => {
+          <Member key={member.id} member={member} />
+        })
+       }
+
+      </div>
+    );
+  };
+
+export default MemberList;
