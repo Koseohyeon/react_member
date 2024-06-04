0914a06235a938eb7e59d64a30332950c6af4b5f
Koseohyeon <ksh030818@naver.com>
2024-06-04 17:22:48 +0900

    이거
    이거

--- src/pages/MemberList.js
+++ src/pages/MemberList.js
@@ -1,25 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import Member from '../components/Member';
-const MemberList = () => {
+import React, { useEffect, useState } from 'react'
+import Member from '../components/Member'
+import axios from 'axios';
 
-    const [member,setMembers]= useState([]);
-    useEffect(()=>{
-      //초기화
+export default function MemberList() {
 
-    },[]);
+  const [members, setMembers] = useState([]);
 
+  useEffect(()=> {
+    // 초기화
+    axios.get("http://localhost:8088/api/memberlist")
+    .then((res) => {
+      console.log("data: ", res.data);
+      setMembers(res.data);
+    })
+    .catch((error) => {
+      console.log("오류 발생!!!!");
+      console.log(error);
+    })
+  }, []);
 
-    return (
-      <div>
+  return (
+    <div>
         <h1>회원 리스트 페이지</h1>
-       {
-        MemberSave.map((member) => {
-          <Member key={member.id} member={member} />
-        })
-       }
-
-      </div>
-    );
-  };
-
-export default MemberList;
+        <div>
+        {
+          members.map((member) => 
+            <Member key={member.id} member={member} />
+          )
+        }
+        </div>
+    </div>
+  )
+}
